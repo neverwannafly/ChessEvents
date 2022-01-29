40cfc681b0b2e19bed3fda5ca6fc88fb90fa9584
neverwannafly <s0.alively@gmail.com>
2022-01-29 15:28:23 +0530

    minor refactor

---
+++ app/javascript/constants/board.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+export const defaultConfig = {
+  coordinates: false,
+  highlight: {
+    lastMove: true,
+    check: true,
+  },
+  animation: {
+    enabled: true,
+    duration: 250,
+  },
+};
